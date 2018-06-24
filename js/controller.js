6436217274e7520e049741969088c12d6c4e3ec0
Dominik Mocher <dominik.mocher@student.tugraz.at>
2018-06-24 14:56:09 +0200

    added saveLocally function

--- js/controller.js
+++ js/controller.js
@@ -1,9 +1,10 @@
+var tokens = [];
+
 function generateTokens() {
     let imageUrl = document.getElementById('imageUrl').value;
     let amount = document.getElementById('amount').value;
     let sizeElement = document.getElementById('size');
     let size = sizeElement.options[sizeElement.selectedIndex].value;
-    console.log("Should generate tokens from "+imageUrl+ " " + amount + " times and size " + size);
     appendTokens(imageUrl, amount, size);
 }
 
@@ -12,6 +13,8 @@ function appendTokens(imageUrl, amount, size) {
     for(var i = 0; i < amount; i++){
         let newToken = generateNewToken(imageUrl, size);
         tokensSpace.appendChild(newToken);
+        tokens.push({imageUrl: imageUrl, amount: amount, size: size});
+        console.log("tokens: " + tokens.length);
     }
 }
 
@@ -47,6 +50,16 @@ function clearAndReset() {
 
 function printTokens() {
     window.print();
+}
+
+function saveLocally(){
+    let text = tokens.map(function (token) {
+        return JSON.stringify(token);
+    }).join();
+    let downloadAnchor = document.createElement('a');
+    downloadAnchor.setAttribute('href', 'data:text/plain;charset=utf-u,'+encodeURIComponent(text));
+    downloadAnchor.setAttribute('download', 'tokens.txt');
+    downloadAnchor.click()
 }
 
 //TODO: Feature request: export to file
